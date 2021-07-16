bf3fb4227ef237fe8a5adb9c95e2bf9bdafec2e1
ayoung0073 <ayong0310@naver.com>
2021-07-16 23:19:11 +0900

    [01] 메서드 참조 연습 코드

---
+++ modern-java-in-action/code/ch01/FilePractice.java
@@ -0,0 +1,21 @@
+package ch01;
+
+import java.io.File;
+import java.util.Arrays;
+
+public class FilePractice {
+    public static void main(String[] args) {
+
+        //        File[] hiddenFiles = new File("../../").listFiles(new FileFilter() {
+        //            public boolean accept(File file) {
+        //                return file.isHidden(); // 숨겨진 파일 필터링
+        //            }
+        //        });
+
+        File[] hiddenFiles = new File("../../").listFiles(File::isHidden);
+
+        assert hiddenFiles != null;
+        Arrays.stream(hiddenFiles).forEach(file -> System.out.println(file.getName()));
+
+    }
+}
