6f44a21b7f4cdb520c491e15b1401717c3acb71d
ayoung0073 <ayong0310@naver.com>
2021-07-24 11:31:44 +0900

    [04] 스트림 소개

---
+++ modern-java-in-action/code/ch04/OneConsumePractice.java
@@ -0,0 +1,14 @@
+package ch04;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class OneConsumePractice {
+    public static void main(String[] args) {
+        List<String> title = Arrays.asList("Modern", "Java8", "In", "Action");
+        Stream<String> s = title.stream();
+        s.forEach(System.out::println);
+        // s.forEach(System.out::println); // Exception in thread "main" java.lang.IllegalStateException: stream has already been operated upon or closed
+    }
+}
